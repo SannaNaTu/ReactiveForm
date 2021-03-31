a6a5b163e4af31e62fd07b63bc19412f17fc38a4
SannaNaTu <31957337+SannaNaTu@users.noreply.github.com>
2021-03-31 23:22:13 +0300

    added form html, ts, css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,18 @@ import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import {RouterModule, Routes} from '@angular/router';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatSelectModule} from '@angular/material/select';
+import {MatNativeDateModule, MatOptionModule} from '@angular/material/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatInputModule} from '@angular/material/input';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+
 
 @NgModule({
   declarations: [
@@ -12,11 +24,26 @@ import {RouterModule, Routes} from '@angular/router';
     FormComponent,
     PageNotFoundComponent
   ],
-    imports: [
-        BrowserModule,
-        RouterModule,
-        AppRoutingModule
-    ],
+  imports: [
+    BrowserModule,
+    RouterModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatCheckboxModule,
+    MatOptionModule,
+    MatAutocompleteModule,
+    MatSelectModule,
+    MatButtonModule,
+    FormsModule,
+    MatExpansionModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+
+
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -6,10 +7,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-
-  constructor() { }
+  profileForm: FormGroup;
+  constructor(private fbuiler: FormBuilder) {}
 
   ngOnInit(): void {
+    this.profileForm = this.fbuiler.group(
+      {
+        firstName: ['', [Validators.required, Validators.minLength(2), Validators.pattern('^[a-zA-Z ]*$')]],
+        lastName: ['', [Validators.required, Validators.minLength(2), Validators.pattern('^[a-zA-Z ]*$')]],
+        personId: ['', [Validators.required,
+          Validators.pattern('^(0[1-9]|[12]\\d|3[01])(0[1-9]|1[0-2])([5-9]\\d+|\\d\\d-|[01]\\dA)\\d{3}[\\dA-Z]$')]],
+        // 120464-126J
+        eMail: ['', [Validators.required, Validators.email]],
+        pickerDate: ['', [Validators.required]],
+        termsAndConditions: ['', [Validators.requiredTrue]]
+      });
   }
 
+  onSubmit() {
+    console.log(this.profileForm);
+
+  }
+
+  get firstName() {
+    return this.profileForm.get('firstName');
+  }
+  get lastName() {
+    return this.profileForm.get('lastName');
+  }
+
+  get personId() {
+    return this.profileForm.get('personId');
+  }
+
+  get eMail() {
+    return this.profileForm.get('eMail');
+  }
+  get pickerDate() {
+    return this.profileForm.get('date');
+  }
+  get termsAndConditions() {
+    return this.profileForm.get('termsAndConditions');
+  }
 }
